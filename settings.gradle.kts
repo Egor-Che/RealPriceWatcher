02f47efd93edcfe0498707321888f60607199282
Egor <30294650+Egor-Che@users.noreply.github.com>
2025-12-29 22:07:27 +0300

    Create settings.gradle.kts
    Создан файл настроек settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+// Файл: RealPriceWatcher/settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RealPriceWatcher"
+include(":app")
